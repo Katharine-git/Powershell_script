ff4fe2b11720da063132d1a37ed2ea215d06ae47
Katharine-git <katharinesheen@gmail.com>
2021-10-13 17:05:10 +0530

    edited

--- Task_5.ps1
+++ Task_5.ps1
@@ -1,3 +1,6 @@
+#This script gets and list all files and folders from the directory where your code resides, prints all files with full path in console. 
+#Read all ps1 files and gets the content and append everything to a new file with *.ps1 extension.
+
 Get-ChildItem -Path 'C:\Users\user\OneDrive\Documents\PS Tasks\'
 $list = get-childitem "C:\Users\user\OneDrive\Documents\PS Tasks\" -recurse |ForEach-Object -Process {$_.FullName}
 ForEach($file in $list)
--- creating_files.ps1
+++ creating_files.ps1
@@ -1,4 +1,6 @@
-﻿$basepath = 'C:\Users\user\OneDrive\Documents\PS Tasks'
+﻿#This script reads a list of file names from a text file abd creates the specified files .txt files
+
+$basepath = 'C:\Users\user\OneDrive\Documents\PS Tasks'
 $name =  Get-Content 'C:\Users\user\OneDrive\Documents\PS Tasks\filename.txt' 
 for($i=0; $i -lt $name.Count; $i++)
 {
