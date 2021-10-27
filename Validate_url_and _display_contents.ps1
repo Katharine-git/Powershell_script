d44ee88f978b0c97b49d1907c3c5422829270b64
Katharine-git <78786279+Katharine-git@users.noreply.github.com>
2021-10-27 18:24:57 +0530

    Update Validate_url_and _display_contents.ps1

--- Validate_url_and _display_contents.ps1
+++ Validate_url_and _display_contents.ps1
@@ -1,4 +1,11 @@
-﻿#Validating url and displaying 
+# Task 1
+# Check the url(https://jsonplaceholder.typicode.com/todos/1) and you need to download the data and present/print those details
+# 1. validate If the website exists download the values from the url
+# 2. Print the user Id and status
+
+# Task 2
+# Get the computer name, the logged in user along with the free space in the computer
+
 Function validate($url)
 {
 
