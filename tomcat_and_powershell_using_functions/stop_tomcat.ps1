8adb6e7bf86d357f2b15766f8b3db924fb89812d
Katharine-git <78786279+Katharine-git@users.noreply.github.com>
2021-10-27 18:22:28 +0530

    Delete stop_tomcat.ps1

--- tomcat_and_powershell_using_functions/stop_tomcat.ps1
+++
@@ -1,9 +0,0 @@
-$path= "C:\Users\user\OneDrive\Documents\PS Tasks\tomcat_and_powershell_2\tomcat.properties"
-$output= Get-content $path| ConvertFrom-StringData
-
-$filelocation=$output.unzip_destination
-$tomcatversion=$output.version
-
-Set-Location -Path $filelocation\$tomcatversion\bin
-
-Start-Process shutdown.bat
