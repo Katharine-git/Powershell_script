547e9866bbf8a50be6ad3264ebab053c78328bd8
Katharine-git <78786279+Katharine-git@users.noreply.github.com>
2021-10-27 18:26:26 +0530

    Delete creating_files.ps1

--- creating_files.ps1
+++
@@ -1,8 +0,0 @@
-﻿#This script reads a list of file names from a text file abd creates the specified files .txt files
-
-$basepath = 'C:\Users\user\OneDrive\Documents\PS Tasks'
-$name =  Get-Content 'C:\Users\user\OneDrive\Documents\PS Tasks\filename.txt' 
-for($i=0; $i -lt $name.Count; $i++)
-{
-New-item -Path ($basepath) -Name ($name[$i]) -ItemType "file" 
-}
